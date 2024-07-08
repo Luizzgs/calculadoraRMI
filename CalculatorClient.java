cd5eb77ceb3969055b3534cf70e31190a4888179
Luizzgs <lgsilvakies@gmail.com>
2024-07-08 20:12:53 -0300

    first commit

--- CalculatorClient.java
+++ CalculatorClient.java
@@ -7,13 +7,13 @@ public class CalculatorClient {
             Registry registry = LocateRegistry.getRegistry("localhost", 1099);
             Calculator calculator = (Calculator) registry.lookup("CalculatorService");
 
-            double a = 10.0;
+            double a = 8.0;
             double b = 5.0;
-
-            System.out.println("Addition: " + calculator.add(a, b));
-            System.out.println("Subtraction: " + calculator.subtract(a, b));
-            System.out.println("Multiplication: " + calculator.multiply(a, b));
-            System.out.println("Division: " + calculator.divide(a, b));
+            System.err.println("Valores: " + a + " e " + b);
+            System.out.println("Adição (+): " + calculator.add(a, b));
+            System.out.println("Subtração (-): " + calculator.subtract(a, b));
+            System.out.println("Multiplicação (*): " + calculator.multiply(a, b));
+            System.out.println("Divisão (/): " + calculator.divide(a, b));
         } catch (Exception e) {
             e.printStackTrace();
         }
